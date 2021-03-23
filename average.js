19b0693889938cef9b77a9e33da3e5a5fa9775c8
cyniaprod <cyniaprod@gmail.com>
2021-03-23 13:23:01 +0100

    adding graph

--- average.js
+++ average.js
@@ -1,9 +1,15 @@
 var container = document.getElementById("area");
 var result = document.getElementById("result");
+var graphContainer = document.getElementById("graphContainer")
 
 var clearBtn = document.getElementById("clear");
 var addBtn = document.getElementById('add');
 var runBtn = document.getElementById("calcul");
+
+var listNotes = [];
+var listMatieres = []
+var listColors = []
+
 
 var counter = 1;
 var id = 1;
@@ -15,6 +21,8 @@ runBtn.addEventListener('click', run);
 function clear() {
     counter = 1;
     container.innerHTML = '<div class="row"><h2>#1</h2><div class="col mb-3"><label for="size" class="form-label">Matiere</label><input class="form-control" id="matiere" placeholder="ex: Français" type="text"/></div><div class="col mb-3"><label for="size" class="form-label">Note</label><input class="form-control" id="note" min="0" placeholder="ex: 2.2" type="number"/></div><div class="col mb-3"><label for="size" class="form-label">Coeff</label><input class="form-control" id="coeff" min="0" placeholder="ex: 6" type="number"/></div></div>'
+    result.innerHTML = "";
+    graphContainer.innerHTML = '<canvas id="myGraph" width="200" height="200"></canvas>'
 }
 
 function add() {
@@ -34,28 +42,46 @@ function add() {
 function remove(event) {
     event.target.parentNode.parentNode.remove();
     counter--;
+
+    for(let i = 1; i < container.children.length; i++) {
+        var element = container.children[i].children[0].innerText = "#" + (i+1);
+    }
 }
 
 function run() {
 
     result.innerHTML = "";
-    
+    graphContainer.innerHTML = '<canvas id="myGraph" width="200" height="200"></canvas>'
+
+    listNotes = [];
+    listMatieres = [];
+    listColors = [];
+
+
     var totalNote = 0;
     var totalCoeff = 0;
 
     for(let i = 0; i < container.children.length; i++) {
         
+        var matiere = container.children[i].children[1].children[1].value;
         var note = container.children[i].children[2].children[1].value;
         var coeff = container.children[i].children[3].children[1].value;
         
+        listNotes.push(note * coeff);
+        listMatieres.push(matiere);
+        listColors.push('#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6));
+
         totalNote += (note * coeff);
-        totalCoeff += +coeff;
+        totalCoeff += +coeff;        
     }
+
+
     var total = (totalNote / totalCoeff).toPrecision(5);
 
     var totalElement = document.createElement('h1');
     totalElement.classList.add("mt-3")
     totalElement.classList.add("text-center")
+
     var className = total < 10 ? "danger" : "success";
     if(total == 10) {
         className = "warning";
@@ -63,5 +89,19 @@ function run() {
 
     totalElement.innerHTML = "Votre moyenne est de <b class='text-"+ className +"'>" + total + "</b>";
     result.appendChild(totalElement);
+
+    var data = {
+        datasets: [{
+            data: listNotes,
+            backgroundColor: listColors
+        }],
+
+        labels: listMatieres
+    }
+    var ctx = document.getElementById('myGraph').getContext('2d');
+    var graph = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+    });
 
 }
